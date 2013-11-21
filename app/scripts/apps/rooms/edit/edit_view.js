91fb04aa408aea7384c78ba5464507c7c7e5cad4
Ronald R. Gemao <ronadmad7@gmail.com>
2013-11-21 15:26:35 +0800

    -removed search filter -attached new edit form -revised room collection (room.js)

--- app/scripts/apps/rooms/edit/edit_view.js
+++ app/scripts/apps/rooms/edit/edit_view.js
@@ -2,15 +2,14 @@ define(["app", "apps/rooms/common/views"], function(MERORS, CommonViews){
   MERORS.module("RoomsApp.Edit.View", function(View, MERORS, Backbone, Marionette, $, _){
     View.Room = CommonViews.Form.extend({
       initialize: function(){
-        this.title = "Edit " + this.model.get("roomName") + " " + this.model.get("capacity");
+        this.title = "Edit " + this.model.get("roomName");
       },
 
       onRender: function(){
         if(this.options.generateTitle){
           var $title = $("<h1>", { text: this.title });
           this.$el.prepend($title);
-        }
-
+        } 
         this.$(".js-submit").text("Update room");
       }
     });
--- app/scripts/apps/rooms/list/templates/panel.tpl
+++ app/scripts/apps/rooms/list/templates/panel.tpl
@@ -1,7 +1,8 @@
 <button class="btn btn-primary js-new">New room</button>
-<form id="filter-form" class="form-search form-inline pull-right">
+<!-- <form id="filter-form" class="form-search form-inline pull-right">
   <div class="input-append">
     <input type="text" class="span2 search-query js-filter-criterion">
     <button type="submit" class="btn">Filter rooms</button>
   </div>
-</form>
+</form> -->
+<!-- Removed by nad -->
