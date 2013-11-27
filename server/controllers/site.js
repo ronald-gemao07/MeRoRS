4f4d90afe4c1f576d5042789706f8ad8ba2dacbd
Nat Yanih <natyanih@gmail.com>
2013-11-27 10:35:24 +0800

    Added title to main page.

--- server/controllers/site.js
+++ server/controllers/site.js
@@ -1,14 +1,13 @@
-var mongoose = require('mongoose')
-  , User = mongoose.model('User')
-  , utils = require('../lib/utils')
+var mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    utils = require('../lib/utils');
 
-exports.index = function (req, res) {
-	if (req.isAuthenticated()) {
-		res.redirect('/app');
-	} else {
-		res.render('index', {
-			title: 'Meeting Room Reservation System'
-		});
-	}
-
-}
+    exports.index = function(req, res) {
+        if (req.isAuthenticated()) {
+            res.redirect('/app');
+        } else {
+            res.render('index', {
+                title: 'Meeting Room Reservation System'
+            });
+        }
+    };
