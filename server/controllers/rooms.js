633e5cdc36cb283b8e6fd4fa7b645993a28a1487
Ronald R. Gemao <ronadmad7@gmail.com>
2013-11-28 13:24:33 +0800

    resolved conflicts

--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -3,19 +3,3 @@ var mongoose = require('mongoose')
   , utils = require('../lib/utils');
 
 
-exports.create = function(req, res) {
-	console.log("POST!");
-    var room = new Room(req.body)
-    room.provider = 'local'
-    room.save(function(err) {
-        if (err) {
-        	alert('error');
-        }
-
-        // manually login the user once successfully signed up
-    })
-}
-
-exports.test = function(req, res) {
-	console.log("GET!");
-}
