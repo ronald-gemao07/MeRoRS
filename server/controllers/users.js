a0a4afc6e0b9d88494f849cbc8e15659cddd11fb
Nat Yanih <natyanih@gmail.com>
2013-12-09 15:59:03 +0800

    Partial implementation of the signup functionality (1451) - implemented the createUser function - added server side validation for the email and password - prettified the code for readability

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,96 +1,85 @@
+// Module Dependencies
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var utils = require('../lib/utils');
 
-/**
- * Module dependencies.
- */
+// Export Functions
 
-var mongoose = require('mongoose')
-  , User = mongoose.model('User')
-  , utils = require('../lib/utils');
-
-var login = function (req, res) {
-  if (req.session.returnTo) {
-    res.redirect(req.session.returnTo)
-    delete req.session.returnTo
-    return
-  }
-  req.user.update();
-  res.redirect('/app')
+var login = function(req, res) {
+    if (req.session.returnTo) {
+        res.redirect(req.session.returnTo);
+        delete req.session.returnTo;
+        return;
+    }
+    req.user.update();
+    res.redirect('/app');
 }
 
-//exports.signin = function (req, res) {
-
-/**
- * Auth callback
- */
-
-exports.loginAction = function(req, res){
+//Login Action
+exports.loginAction = function(req, res) {
     console.log(req.body);
     res.send(200, "OK");
     res.end();
 }
 
-//
-// Show Sign Up Form
-//
 
-exports.authCallback = login
+exports.authCallback = login;
 
 /**
  * Show login form
  */
 
-exports.login = function (req, res) {
-/*  if (req.isAuthenticated()) {
+exports.login = function(req, res) {
+    /*  if (req.isAuthenticated()) {
     res.redirect('/feeds');
     return;
   }
 */
-  res.render('users/login', {
-    title: 'Login',
-    message: req.flash('error')
-  })
+    res.render('users/login', {
+        title: 'Login',
+        message: req.flash('error')
+    })
 }
 
-/**
- * Show sign up form
- */
 
-exports.signup = function (req, res) {
-  if (req.isAuthenticated()) {
-    res.redirect('/app');
-    return;
-  }
+// Signup Page
+exports.signup = function(req, res) {
+    if (req.isAuthenticated()) {
+        res.redirect('/app');
+        return;
+    }
 
-  res.render('user/signup', {
-    title: 'Sign up'
-  })
+    res.render('user/signup', {
+        title: 'Sign up'
+    })
 }
 
 // Help Page
-
-exports.help = function (req, res) {
-  if (req.isAuthenticated()) {
-      res.redirect('/app');
-      return;
+exports.help = function(req, res) {
+    if (req.isAuthenticated()) {
+        res.redirect('/app');
+        return;
     }
 
-  res.render('user/help', {
-    title: 'Help Page'
-  })
+    res.render('user/help', {
+        title: 'Help Page'
+    })
 }
 
-// Process forgot password
-exports.forgotPassword = function(req, res){
-  if (req.isAuthenticated()) {
+// Forgot password Page
+exports.forgotPassword = function(req, res) {
+    if (req.isAuthenticated()) {
         res.redirect('/app');
         return;
     }
 
     res.render('user/forgot-password', {
-      title: 'Forgot Password'
+        title: 'Forgot Password'
     })
 }
 
+
+// Action for Forgot Password
 exports.resetPassword = function(req, res) {
     if (req.isAuthenticated()) {
         res.redirect('/app');
@@ -102,69 +91,144 @@ exports.resetPassword = function(req, res) {
     res.end();
 }
 
-/**
- * Logout
- */
-
-exports.logout = function (req, res) {
-  req.logout()
-  res.redirect('/login')
+// Action for Logout
+exports.logout = function(req, res) {
+    req.logout()
+    res.redirect('/login')
 }
 
-/**
- * Session
- */
-
-/**
- * Create user
- */
-
+// Action for Signup - Create User
 exports.createUser = function(req, res) {
-    var user = new User(req.body)
-    console.log(user);
-    user.provider = 'local'
-    user.save(function(err) {
-        if (err) {
-            return res.render('user/signup', {
-                errors: utils.errors(err.errors),
-                user: user,
-                title: 'Sign up'
-            })
-        }
 
-        // manually login the user once successfully signed up
-        req.logIn(user, function(err) {
-            if (err) return next(err)
-            return res.redirect('/app');
-        })
-    })
+    // Validate inputs
+    // Code needs refactor later
+    // Check if email is in use
+
+    User.findOne({
+        email: req.body.inputEmail
+    }, function(err, user) {
+        if (err) {
+            res.send(403, "error");
+            res.end();
+        }
+        if (user) { // email in use
+            res.send(403, "Email in use.");
+            res.end();
+        } else {
+            // Email not in use, check password here
+            if ((req.body.inputPassword != req.body.confirmPassword) || req.body.inputPassword.length < 6) {
+                res.send(403, "Password do not match or less than minimum length. ");
+                res.end();
+            } else {
+
+                // Email and password is valid
+                // Extract info from req.body
+                var userEmail = req.body.inputEmail;
+                var userPassword = req.body.inputPassword;
+
+                var userFirstName = req.body.inputEmail.slice(0, req.body.inputEmail.indexOf("."));
+                userFirstName = userFirstName.substr(0, 1).toUpperCase() + userFirstName.substr(1);
+
+                var userLastName = req.body.inputEmail.slice(req.body.inputEmail.indexOf(".") + 1, req.body.inputEmail.indexOf("@"));
+                userLastName = userLastName.substr(0, 1).toUpperCase() + userLastName.substr(1);
+
+                var userDetails = {
+                    email: userEmail,
+                    password: userPassword,
+                    firstName: userFirstName,
+                    lastName: userLastName
+                }
+
+                var user = new User(userDetails);
+
+                user.provider = 'local';
+
+                user.save(function(err) {
+                    if (err) {
+                        res.send(403, "Error saving details.");
+                        res.end();
+                    } else { // save successful, force login
+                        req.logIn(user, function(err) {
+                            if (err) {
+                                return next(err);
+                            } else {
+                                return res.redirect('/app');
+                            }
+                        })
+                    }
+                });
+
+            }
+        }
+    });
+
+    // Extract info from req.body
+
+
+    // var user = new User(req.body);
+
+    // user.provider = 'local'
+    // user.save(function(err) {
+    //     if (err) {
+    //         return res.render('user/signup', {
+    //             errors: utils.errors(err.errors),
+    //             user: user,
+    //             title: 'Sign up'
+    //         })
+    //     }
+
+    //     // manually login the user once successfully signed up
+    //     req.logIn(user, function(err) {
+    //         if (err) return next(err)
+    //         return res.redirect('/app');
+    //     })
+    // })
     // res.send(200, "OK");
     // res.end();
 }
 
-/**
- *  Show profile
- */
 
-exports.show = function (req, res) {
-  var user = req.profile
-  res.render('users/show', {
-    title: user.name,
-    user: user
-  })
+// Server validation for Signup Inputs
+var validate = function(input) {
+    // Check DB if email is in use
+    User.findOne({
+        email: input.inputEmail
+    }, function(err, user) {
+        if (err) {
+            console.log("Here at err:");
+            return false;
+        }
+        if (user) {
+            console.log("Here at user");
+            return false;
+        }
+        return true;
+    });
+}
+
+
+// Show Profile
+exports.show = function(req, res) {
+    var user = req.profile
+    res.render('users/show', {
+        title: user.name,
+        user: user
+    })
 }
 
 /**
  * Find user by id
  */
 
-exports.user = function (req, res, next, id) {
-  User
-    .findOne({ _id : id })
-    .exec(function (err, user) {
-      if (err) return next(err)
-      if (!user) return next(new Error('Failed to load User ' + id))
-      req.profile = user
-      next()
-    })
+exports.user = function(req, res, next, id) {
+    User
+        .findOne({
+            _id: id
+        })
+        .exec(function(err, user) {
+            if (err) return next(err)
+            if (!user) return next(new Error('Failed to load User ' + id))
+            req.profile = user
+            next()
+        })
 }
