6be2518e51f6134b003726a20b587a9d26e34813
Nat Yanih <natyanih@gmail.com>
2013-12-10 08:56:17 +0800

    Code formatting changes due to html-prettify

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,44 +1,54 @@
-var mongoose = require('mongoose')
-  , LocalStrategy = require('passport-local').Strategy
-  , User = mongoose.model('User')
-  , auth = require('./middlewares/authorization')
+var mongoose = require('mongoose'),
+    LocalStrategy = require('passport-local').Strategy,
+    User = mongoose.model('User'),
+    auth = require('./middlewares/authorization')
 
 
-module.exports = function (passport, config) {
-  // require('./initializer')
+    module.exports = function(passport, config) {
+        // require('./initializer')
 
-  // serialize sessions
-  passport.serializeUser(function(user, done) {
-    done(null, user.id)
-  })
+        // serialize sessions
+        passport.serializeUser(function(user, done) {
+            done(null, user.id)
+        })
 
-  passport.deserializeUser(function(id, done) {
-    User.findOne({ _id: id }, function (err, user) {
-      done(err, user)
-    })
-  })
+        passport.deserializeUser(function(id, done) {
+            User.findOne({
+                _id: id
+            }, function(err, user) {
+                done(err, user)
+            })
+        })
 
-  // use local strategy
-  passport.use(new LocalStrategy({
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true
-    },
-    function(req, email, password, done) {
-      passport;
-      auth;
-      req;
-      arguments;
-      User.findOne({ email: email }, function (err, user) {
-        if (err) { return done(err) }
-        if (!user) {
-          return done(null, false, { message: 'Unknown user' })
-        }
-        if (!user.authenticate(password)) {
-          return done(null, false, { message: 'Invalid password' })
-        }
-        return done(null, user)
-      })
+        // use local strategy
+        passport.use(new LocalStrategy({
+                usernameField: 'inputEmail',
+                passwordField: 'inputPassword',
+                passReqToCallback: true
+            },
+            function(req, email, password, done) {
+                passport;
+                auth;
+                req;
+                arguments;
+                User.findOne({
+                    email: email
+                }, function(err, user) {
+                    if (err) {
+                        return done(err)
+                    }
+                    if (!user) {
+                        return done(null, false, {
+                            message: 'Unknown user'
+                        })
+                    }
+                    if (!user.authenticate(password)) {
+                        return done(null, false, {
+                            message: 'Invalid password'
+                        })
+                    }
+                    return done(null, user)
+                })
+            }
+        ));
     }
-  ));
-}
