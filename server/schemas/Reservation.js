d95d61cea58de7c130e853edc6a1fe75d411f031
Ronald R. Gemao <ronadmad7@gmail.com>
2013-12-10 14:53:43 +0800

    Revised Reservation Schema(1455) 	-removed unused objects in schema (1530) 	-removed repetition logic as for now (1530)

--- server/schemas/Reservation.js
+++ server/schemas/Reservation.js
@@ -1,17 +1,21 @@
 var mongoose = require( 'mongoose' ),
     Schema = mongoose.Schema;
 
-var ReservationSchema = new Schema({
-    room: { type: String, default: '' },
-    reservedBy: { type: String, default: '' },
-    title: { type: String, default: '' },
-    description: { type: String, default: '' },
-    startDate: { type: Date, default: '' },
-    endDate: { type: Date, default: '' },
-    startTime: { type: String, default: '' },
-    endTime: { type: String, default: '' },
-    repeatType: { type: String, default: 'one time' },
-    dayOfTheWeek: {type: Array , default:[] }
+var ReservationSchema= new Schema({
+    roomName:{ type: String , default:'' },
+    roomId: { type: String , default:'' },
+    reservedBy: { type: String , default:'' },
+    title: { type: String , default:'' },
+    description: { type: String , default:'' },
+    startTime: { type: Date , default:'' },
+    endTime: { type: Date , default:'' }
+    /*repetition: {
+        type: { type: String , default:'' },
+        endTime: { type: String , default:'' },
+        endDate: { type: Date , default:'' },
+        days: { type: Array , default:[] }
+    }*/
+
 });
 
 ReservationSchema.pre('save', function (next) {
