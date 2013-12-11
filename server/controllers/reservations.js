7a5f25d0c5e3d98fa3fa674e0e9028c5d29c5b0a
Ronald R. Gemao <ronadmad7@gmail.com>
2013-12-11 10:06:06 +0800

    Removed Reservation Details (1455) 	- removed ReservationDetails.js under Schema folder (1083) 	- excluded ReservationDetails schema from the  reservation controller (1083)

--- server/controllers/reservations.js
+++ server/controllers/reservations.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose'),
   Reservation = mongoose.model('Reservation'),
-  ReservationDetails = mongoose.model('ReservationDetails'),
   utils = require('../lib/utils');
 
 /* add your methods here...please refer to users.js */
