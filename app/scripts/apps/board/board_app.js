3b8145ac86f979525ba3a892a3499e747072a8b6
Vincent John Cagay <vjcagay@gmail.com>
2013-12-11 09:34:37 +0800

    Organized code
    - Moved calendar function to show_controller.js

--- app/scripts/apps/board/board_app.js
+++ app/scripts/apps/board/board_app.js
@@ -1,198 +1,31 @@
-define(["app"], function(MERORS){
-  MERORS.module("BoardApp", function(BoardApp, MERORS, Backbone, Marionette, $, _){
-    BoardApp.startWithParent = false;
-
-    BoardApp.onStart = function(){
-      console.log("starting BoardApp");
-    };
-
-    BoardApp.onStop = function(){
-      console.log("stopping BoardApp");
-    };
+define(["app", "marionette"], function(MERORS, Marionette){
+  var Router = Marionette.AppRouter.extend({
+    appRoutes: {
+      "board" : "showBoard"
+    }
   });
 
-  MERORS.module("Routers.BoardApp", function(BoardAppRouter, MERORS, Backbone, Marionette, $, _){
-    BoardAppRouter.Router = Marionette.AppRouter.extend({
-        appRoutes: {
-          "board" : "showBoard"
-        }
+  var API = {
+    showBoard: function(){
+      require(["apps/board/show/show_controller"], function(ShowController){
+        $(document).attr("title", "Reservation Board - Global Zeal Meeting Room Reservation System");
+        MERORS.startSubApp(null);
+        ShowController.showBoard();
+        MERORS.execute("set:active:header", "board");
       });
+    }
+  };
 
-        function isCheckOverlap(event){
-            var start = new Date(event.start);
-            var end = new Date(event.end);
+  MERORS.on("board:show", function(){
+    MERORS.navigate("board");
+    API.showBoard();
+  });
 
-            var events = calendar.fullCalendar('clientEvents');
-            for (var i = 0; i < events.length; i++) {
-                var someEvent = events[i];
-
-                if (someEvent._id == event._id)
-                {
-                    continue;
-                }
-
-                var seStart = new Date(someEvent.start);
-                var seEnd = new Date(someEvent.end);
-
-                if ((event.resourceId == someEvent.resourceId)&&(start < seEnd) && (seStart < end)) {// dates overlap
-                    return true;
-                }
-            }
-
-        } 
-
-        var executeAction = function(action, arg){
-          MERORS.startSubApp("BoardApp");
-          action(arg);
-          MERORS.execute("set:active:header", "board");
-        };
-
-        var calendar;
-
-        var API = {
-        showBoard: function(){
-            require(["apps/board/show/show_controller"], function(ShowController){
-                // Pass the events on render
-                // hook up the calendar events
-                // Select or create reservation
-
-                var config = {
-                    select: API.select,
-                    eventResize: API.eventResize,
-                    eventDrop: API.eventDrop,
-                    eventClick: API.eventClick
-                }
-
-                executeAction(function  () {}, null);                      
-
-                $.when(API.getRooms()).done(function (rooms) {
-                    config.resources = rooms;
-                    calendar = ShowController.showBoard(config);
-                    console.log('show calendar')
-                    console.log(rooms)
-                });
-            });           
-        }, 
-        getRooms: function (config) {       
-            var dfd = $.Deferred(); 
-            require(["common/views", "entities/room"], function(CommonViews){
-                var loadingView = new CommonViews.Loading({
-                    title: "Loading...",
-                    message: "Loading events and rooms."
-                  });
-                  MERORS.mainRegion.show(loadingView);
-
-                var fetchingRooms = MERORS.request("room:entities");
-
-                $.when(fetchingRooms).done(function(rooms){
-                    console.log('got rooms')
-                    var resources = [];
-                    rooms.each(function (room) {
-                        resources[resources.length] = {
-                            id: room.get('_id'),
-                            name: room.get('room')
-                        }
-                    });
-                    dfd.resolve(resources);
-                });
-            });
-
-            return dfd.promise();
-        }, 
-        select: function(start, end, allDay, event, resourceId) {
-            console.log('calendar select');
-            console.log(arguments)
-           $("#dialog").dialog({
-                resizable: true,
-                title: 'Create New Reservation',
-                width: "auto",
-                buttons: {
-                    CANCEL: function() {
-                        $("#dialog").dialog("close");
-                    },
-                    "ADD": function(ev) {   
-                        var form = $('#test-form');
-                        if(title=form.find('input[id=title]').val()){
-                            calendar.fullCalendar('renderEvent', {
-                                    title: title,
-                                    start: start,
-                                    end: end,
-                                    allDay: allDay,
-                                    resourceId: resourceId
-                                },true 
-                            );
-                        }
-                        $("#dialog").dialog("close");
-                    }
-                }
-            });
-            calendar.fullCalendar('unselect');
-        }, 
-        eventClick: function(calEvent, jsEvent, view) {
-            var id = calEvent.id;
-            $("#dialog").dialog({
-                resizable: true,
-                title: 'Edit Reservation',
-                width: "auto",
-                buttons: {
-                    CANCEL: function() {
-                        $("#dialog").dialog("close");
-                    },
-
-                    "Update": function() {
-                        var form =$('#test-form');
-                        console.log(calEvent.title);
-                        calEvent.title = form.find('input[id=title]').val();
-                        if(title=form.find('input[id=title]').val()){
-                            calendar.fullCalendar('updateEvent', {
-                                    title: calEvent.title,
-                                    start: calEvent.start,
-                                    end: calEvent.end,
-                                    allDay: calEvent.allDay,
-                                    resourceId: calEvent.resourceId
-                                },true 
-                            );
-                        }
-                        $("#dialog").dialog("close");
-                    },
-
-                    "DELETE": function() {
-                        calendar.fullCalendar('removeEvents', calEvent._id);
-                        $("#dialog").dialog("close");
-                    }
-                }
-            });
-            calendar.fullCalendar('unselect');
-        },
-
-        eventResize: function(event, dayDelta, minuteDelta, revertFunc, jsEvent, ui, view) {
-            console.log("Hello");
-            if(isCheckOverlap(event)){
-
-                revertFunc();
-            }
-
-        },
-        eventDrop: function(event,dayDelta,minuteDelta,allDay,revertFunc) {
-            if(isCheckOverlap(event)){
-                event.resourceId = event.oldResourceId;
-                revertFunc();
-            }
-        }
-      };
-
-      MERORS.on("board:show", function(){
-        MERORS.navigate("board");
-        API.showBoard();
-      });
-
-      MERORS.addInitializer(function(){
-        new BoardAppRouter.Router({
-          controller: API
-        });
-      });
-
-      return MERORS.BoardAppRouter;
-
+  MERORS.addInitializer(function(){
+    new Router({
+      controller: API
     });
+  });
+
+  return Router;
 });
--- app/scripts/apps/board/show/show_controller.js
+++ app/scripts/apps/board/show/show_controller.js
@@ -13,6 +13,97 @@ define(["app", "apps/board/show/show_view", "fullcalendar"], function(MERORS, Vi
 			var m = date.getMonth();
 			var y = date.getFullYear();
 
+			var events = [
+	                {
+	                    title: "All Day Event 1",
+	                    start: new Date(y, m, d - 1),
+	                    end: new Date(y, m, d + 1),
+	                    resourceId: "resource1"
+	                },
+	                {
+	                    title: "Past Event 1",
+	                    start: new Date(y, m, 8, 8, 30),
+	                    end: new Date(y, m, 8, 9, 30),
+	                    allDay: false,
+	                    resourceId: "resource1"
+	                },
+	                {
+	                    title: "Short Event 1",
+	                    start: new Date(y, m, d, 8, 30),
+	                    end: new Date(y, m, d, 9, 30),
+	                    allDay: false,
+	                    resourceId: "resource1"
+	                },
+	                {
+	                    title: "Short Event 2",
+	                    start: new Date(y, m, d + 1, 14, 00),
+	                    end: new Date(y, m, d + 1, 15, 00),
+	                    allDay: false,
+	                    resourceId: "resource1"
+	                },
+	                {
+	                    title: "All Day Event 2",
+	                    start: new Date(y, m, d - 2),
+	                    end: new Date(y, m, d - 1),
+	                    resourceId: "resource2"
+	                },
+	                {
+	                    title: "Lunch",
+	                    start: new Date(y, m, d, 12, 0),
+	                    end: new Date(y, m, d, 14, 0),
+	                    allDay: false,
+	                    resourceId: "resource2"
+	                },
+	                {
+	                    title: "All Day Event 3",
+	                    start: new Date(y, m, d),
+	                    resourceId: "resource3"
+	                },
+	                {
+	                    title: "Click for Google",
+	                    start: new Date(y, m, d, 15, 0),
+	                    end: new Date(y, m, d, 16, 24),
+	                    allDay: false,
+	                    resourceId: "resource3"
+	                },
+	                {
+	                    title: "Room1",
+	                    start: new Date(y, m, d, 15, 0),
+	                    end: new Date(y, m, d, 18, 0),
+	                    allDay: false,
+	                    resourceId: "resource1"
+	                },
+	                {
+	                    title: "Room2",
+	                    start: new Date(y, m, d, 15, 0),
+	                    end: new Date(y, m, d, 18, 0),
+	                    allDay: false,
+	                    resourceId: "resource2"
+	                },
+	                {
+	                    title: "Room3",
+	                    start: new Date(y, m, d, 15, 0),
+	                    end: new Date(y, m, d, 18, 0),
+	                    allDay: false,
+	                    resourceId: "resource3"
+	                }
+	            ];
+
+	        //loop each through all events fetched then modify its editable flag depending on the current time
+	        //if an event's end time is past the current time then it is considered done thus can no longer be edited
+	        events.forEach(function (event){
+	            var currentTime = $.fullCalendar.formatDate(new Date(),'yyyy-MM-dd HH:mm');
+	            var eventEndTime = $.fullCalendar.formatDate(event.end,'yyyy-MM-dd HH:mm');
+	            if(eventEndTime <= currentTime) {
+	                event.editable = false;
+	                event.title += " (Done)";
+	                event.backgroundColor = "#C8DEAB";
+	                event.borderColor = "#C8DEAB";
+	            } else {
+	                //event.eventColor = "#8dc640";
+	            }
+	        });
+
 			var config = {
 			    theme: false,
 			    header: {
@@ -45,54 +136,131 @@ define(["app", "apps/board/show/show_view", "fullcalendar"], function(MERORS, Vi
 			            id: "resource3"
 			        }
 			    ],
-			    events: [
-			        {
-			            title: "All Day Event 1",
-			            start: new Date(y, m, d - 1),
-			            end: new Date(y, m, d + 1),
-			            resourceId: "resource1"
-			        },
-			        {
-			            title: "Short Event 1",
-			            start: new Date(y, m, d, 8, 30),
-			            end: new Date(y, m, d, 9, 30),
-			            allDay: false,
-			            resourceId: "resource1"
-			        },
-			        {
-			            title: "Short Event 2",
-			            start: new Date(y, m, d + 1, 14, 00),
-			            end: new Date(y, m, d + 1, 15, 00),
-			            allDay: false,
-			            resourceId: "resource1"
-			        },
-			        {
-			            title: "All Day Event 2",
-			            start: new Date(y, m, d - 2),
-			            end: new Date(y, m, d - 1),
-			            resourceId: "resource2"
-			        },
-			        {
-			            title: "Lunch",
-			            start: new Date(y, m, d, 12, 0),
-			            end: new Date(y, m, d, 14, 0),
-			            allDay: false,
-			            resourceId: "resource2"
-			        },
-			        {
-			            title: "All Day Event 3",
-			            start: new Date(y, m, d),
-			            resourceId: "resource3"
-			        },
-			        {
-			            title: "Click for Google",
-			            start: new Date(y, m, d, 16, 0),
-			            end: new Date(y, m, d, 18, 30),
-			            allDay: false,
-			            url: "http://google.com/",
-			            resourceId: "resource3"
-			        }
-			    ]		    
+	            events: events,
+	            /*editable: false,*/
+	            select: function(start, end, allDay, event, resourceId) {
+
+	                var currentTime = $.fullCalendar.formatDate(new Date(),'yyyy-MM-dd HH:mm');
+	                var selectedTime = $.fullCalendar.formatDate(start,'yyyy-MM-dd HH:mm');
+
+	                if(selectedTime > currentTime) {
+	                    $("#dialog").dialog({
+	                        resizable: true,
+	                        title: 'Create New Reservation',
+	                        width: "auto",
+	                        buttons: {
+	                            Add: function(ev) {   
+	                                var form = $('#test-form');
+	                                if(title=form.find('input[id=title]').val()){
+	                                    calendar.fullCalendar('renderEvent', {
+	                                            title: title,
+	                                            start: start,
+	                                            end: end,
+	                                            allDay: allDay,
+	                                            resourceId: resourceId
+	                                        },true 
+	                                    );
+	                                }
+	                                $("#dialog").dialog("close");
+	                            },
+	                            Cancel: function() {
+	                                $("#dialog").dialog("close");
+	                            }
+	                        }
+	                    });
+	                } else {
+	                    $("<div>You can only add events later than the current time.</div>").dialog({
+	                        modal: true,
+	                        title: "Create New Reservation"
+	                    });
+	                }
+	               
+	                calendar.fullCalendar('unselect');
+	            },
+	           
+	            eventClick: function(calEvent, jsEvent, view) {
+	                //use current time as base
+	                var currentTime = $.fullCalendar.formatDate(new Date(),'yyyy-MM-dd HH:mm');
+	                //use new event's end time based on the location clicked
+	                var selectedEventEndTime = $.fullCalendar.formatDate(calEvent.end,'yyyy-MM-dd HH:mm');
+
+	                //if the new event's end time is later than current time then it is valid
+	                if(selectedEventEndTime > currentTime) {
+	                    var id = calEvent.id;
+	                    $("#dialog").dialog({
+	                        resizable: true,
+	                        title: 'Edit Reservation',
+	                        width: "auto",
+	                        buttons: {
+
+	                            Update: function() {
+	                                var form = $('#test-form');
+	                                console.log(calEvent.title);
+	                                calEvent.title = form.find('input[id=title]').val();
+	                                if(title=form.find('input[id=title]').val()){
+	                                    calendar.fullCalendar('updateEvent', {
+	                                            title: calEvent.title,
+	                                            start: calEvent.start,
+	                                            end: calEvent.end,
+	                                            allDay: calEvent.allDay,
+	                                            resourceId: calEvent.resourceId
+	                                        },true 
+	                                    );
+	                                }
+	                                $("#dialog").dialog("close");
+	                            },
+
+	                            Delete: function() {
+	                                calendar.fullCalendar('removeEvents', calEvent._id);
+	                                $("#dialog").dialog("close");
+	                            },
+
+	                            Cancel: function() {
+	                                $("#dialog").dialog("close");
+	                            }
+	                        }
+	                    });
+	                } else { //You can not make new reservations in the past, only in the future
+	                    $("<div>You can only edit events that have not yet been accomplished or is currently going on.</div>").dialog({
+	                        modal: true,
+	                        title: "Edit Reservation"
+	                    });
+	                }
+	                
+
+	                calendar.fullCalendar('unselect');
+	            },
+
+	            eventResize: function(event, dayDelta, minuteDelta, revertFunc) {
+	                //use current time as base
+	                var currentTime = $.fullCalendar.formatDate(new Date(),'yyyy-MM-dd HH:mm');
+	                //use the edited event's new end time as base
+	                var selectedEventNewEndTime = $.fullCalendar.formatDate(event.end,'yyyy-MM-dd HH:mm');
+	                //you can only set end times in the future for events
+	                if(currentTime > selectedEventNewEndTime){
+	                    $("<div>You can not resize events with new end times in the past (End time earlier than current).</div>").dialog({
+	                        modal: true,
+	                        title: "Edit Reservation"
+	                    });
+	                    //revert the event's original end time
+	                    revertFunc();
+	                }
+	            },
+	            eventDrop: function( event, dayDelta, minuteDelta, allDay, revertFunc) {
+	                //use current time as base
+	                var currentTime = $.fullCalendar.formatDate(new Date(),'yyyy-MM-dd HH:mm');
+	                //use the edited event's new end time as base
+	                var selectedEventNewEndTime = $.fullCalendar.formatDate(event.end,'yyyy-MM-dd HH:mm');
+	                //you can only set end times in the future for events
+	                if(currentTime > selectedEventNewEndTime){
+	                    $("<div>You can not drag events back into the past (End time earlier than current).</div>").dialog({
+	                        modal: true,
+	                        title: "Drag Reservation"
+	                    });
+	                    //revert the event's original end time
+	                    revertFunc();
+	                }
+	            }		    
 			  		    
 			};
 
@@ -109,8 +277,8 @@ define(["app", "apps/board/show/show_view", "fullcalendar"], function(MERORS, Vi
 			});
 
 			$(".fc-button-prev, .fc-button-next").click("click", function () {
-			var currentFullCalendarDate = $(calendarSelector).fullCalendar("getDate").getFullYear() + "-" + ($('#calendar').fullCalendar("getDate").getMonth() + 1) + "-" + $('#calendar').fullCalendar("getDate").getDate();
-			$("#date-picker").datepicker("setDate", currentFullCalendarDate);
+				var currentFullCalendarDate = $(calendarSelector).fullCalendar("getDate").getFullYear() + "-" + ($('#calendar').fullCalendar("getDate").getMonth() + 1) + "-" + $('#calendar').fullCalendar("getDate").getDate();
+				$("#date-picker").datepicker("setDate", currentFullCalendarDate);
 			});
 
 	    	return calendar;
