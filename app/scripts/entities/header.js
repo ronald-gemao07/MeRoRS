9a90987951339363d6b4409a5efbc16fd16efdff
Nat Yanih <natyanih@gmail.com>
2013-12-19 15:06:52 +0800

    Added page restrictions for logged in users and admins. - Users will no longer be able to view links for Manage Users and Manage Rooms.

--- app/scripts/entities/header.js
+++ app/scripts/entities/header.js
@@ -1,46 +1,81 @@
 'use strict';
 
-define(['app', 'backbone.picky'], function(MERORS){
-  MERORS.module('Entities', function(Entities, MERORS, Backbone, Marionette, $, _){
-    Entities.Header = Backbone.Model.extend({
-      initialize: function(){
-        var selectable = new Backbone.Picky.Selectable(this);
-        _.extend(this, selectable);
-      }
+define(['app', 'backbone.picky'], function(MERORS) {
+    MERORS.module('Entities', function(Entities, MERORS, Backbone, Marionette, $, _) {
+        Entities.Header = Backbone.Model.extend({
+            initialize: function() {
+                var selectable = new Backbone.Picky.Selectable(this);
+                _.extend(this, selectable);
+            }
+        });
+
+        Entities.HeaderCollection = Backbone.Collection.extend({
+            model: Entities.Header,
+
+            initialize: function() {
+                var singleSelect = new Backbone.Picky.SingleSelect(this);
+                _.extend(this, singleSelect);
+            }
+        });
+
+        var initializeHeaders = function() {
+            Entities.headers = new Entities.HeaderCollection([{
+                name: 'My Reservations',
+                url: 'reservations',
+                navigationTrigger: 'reservations:list'
+            }, {
+                name: 'Reservation Board',
+                url: 'board',
+                navigationTrigger: 'board:show'
+            }, {
+                name: 'About',
+                url: 'about',
+                navigationTrigger: 'about:show'
+            }]);
+        };
+
+        var initializeAdminHeaders = function() {
+            Entities.headers = new Entities.HeaderCollection([{
+                name: 'My Reservations',
+                url: 'reservations',
+                navigationTrigger: 'reservations:list'
+            }, {
+                name: 'Reservation Board',
+                url: 'board',
+                navigationTrigger: 'board:show'
+            }, {
+                name: 'Manage Rooms',
+                url: 'rooms',
+                navigationTrigger: 'rooms:list'
+            }, {
+                name: 'Manage Users',
+                url: 'users',
+                navigationTrigger: 'users:list'
+            }, {
+                name: 'About',
+                url: 'about',
+                navigationTrigger: 'about:show'
+            }]);
+        };
+
+        var API = {
+            getHeaders: function() {
+                var profile = MERORS.request('profile:entity:first');
+                var group = profile.get('group');
+
+                if (Entities.headers === undefined && group === 'Administrator') {
+                  initializeAdminHeaders();
+                } else{
+                  initializeHeaders();
+                }
+                return Entities.headers;
+            }
+        };
+
+        MERORS.reqres.setHandler('header:entities', function() {
+            return API.getHeaders();
+        });
     });
 
-    Entities.HeaderCollection = Backbone.Collection.extend({
-      model: Entities.Header,
-
-      initialize: function(){
-        var singleSelect = new Backbone.Picky.SingleSelect(this);
-        _.extend(this, singleSelect);
-      }
-    });
-
-    var initializeHeaders = function(){
-      Entities.headers = new Entities.HeaderCollection([
-        { name: 'My Reservations', url: 'reservations', navigationTrigger: 'reservations:list' },
-        { name: 'Reservation Board', url: 'board', navigationTrigger: 'board:show' },
-        { name: 'Manage Rooms', url: 'rooms', navigationTrigger: 'rooms:list' },
-        { name: 'Manage Users', url: 'users', navigationTrigger: 'users:list' },
-        { name: 'About', url: 'about', navigationTrigger: 'about:show' }
-      ]);
-    };
-
-    var API = {
-      getHeaders: function(){
-        if(Entities.headers === undefined){
-          initializeHeaders();
-        }
-        return Entities.headers;
-      }
-    };
-
-    MERORS.reqres.setHandler('header:entities', function(){
-      return API.getHeaders();
-    });
-  });
-
-  return ;
+    return;
 });
