561b998dee45110179e5eb88232aa3c97832835c
Vince Elizaga <vncelizaga@gmail.com>
2013-12-20 10:18:46 +0800

    Fixed simultaneous creation of reservation bug (1634)

--- app/scripts/apps/board/new/new_view_controller.js
+++ app/scripts/apps/board/new/new_view_controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 define(['app', 'apps/board/new/new_view', 'fullcalendar'], function(MERORS, View) {
-    MERORS.module('BoardApp.NewView', function(NewView, MERORS, $, _) {
+    MERORS.module('BoardApp.NewView', function(NewView, MERORS, Backbone, Marionette, $, _) {
         NewView.Controller = {
             showAddView: function(calObj) {
                 var view = new View.AddView(calObj);
@@ -9,9 +9,9 @@ define(['app', 'apps/board/new/new_view', 'fullcalendar'], function(MERORS, View
                 require(['entities/reservation', 'entities/profile'], function() {
                     var newReservation = MERORS.request('reservation:entity:new');
                     view.on('form:submit', function(data) {
-                        
+
                         // Add the user profile on reservation
-                        var userProfile = MERORS.request('profile:entity:first');                        
+                        var userProfile = MERORS.request('profile:entity:first');
 
                         data.reservedBy = {
                             _id: userProfile.get('_id'),
@@ -24,24 +24,31 @@ define(['app', 'apps/board/new/new_view', 'fullcalendar'], function(MERORS, View
 
                         newReservation.save({}, {
                             success: function(model, response) {
-                                
-                                data.title = data.title.replace(/[^\w\s,.]/gi, '');
-                                data.description = data.description.replace(/[^\w\s,.]/gi, '');
 
-                                calObj.calendar.fullCalendar('renderEvent', {
-                                    title: data.title,
-                                    description: data.description,
-                                    start: calObj.start,
-                                    end: calObj.end,
-                                    allDay: false,
-                                    resourceId: calObj.resourceId,
-                                    reservationId: response._id,
-                                    reservedBy: data.reservedBy,
-                                    description: data.description
-                                }, true);
+                                if (response.results) {
+                                    view.trigger("dialog:close");
+                                    $('<div>Sorry this time slot is already taken.</div>').dialog({
+                                        modal: true,
+                                        title: 'Create New Reservation'
+                                    });
+                                    
+                                } else {
+                                    data.title = data.title.replace(/[^\w\s,.]/gi, '');
+                                    data.description = data.description.replace(/[^\w\s,.]/gi, '');
 
-                                view.trigger("dialog:close");
+                                    calObj.calendar.fullCalendar('renderEvent', {
+                                        title: data.title,
+                                        start: calObj.start,
+                                        end: calObj.end,
+                                        allDay: false,
+                                        resourceId: calObj.resourceId,
+                                        reservationId: response._id,
+                                        reservedBy: data.reservedBy,
+                                        description: data.description
+                                    }, true);
 
+                                    view.trigger("dialog:close");
+                                }
                             },
                             error: function(model, response) {
                                 console.log('error! ' + response);
